0957d7b6fb4805f2c3dd6805d762a1b065e4e160
KanaeOhta <kanae5321@gmail.com>
2020-05-16 23:13:43 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='texttools',
+    version='0.0.1',
+    author='Kanae Ohta',
+    # author_email='',
+    url='https://github.com/KanaeOhta/myutils.git',
+    descriptions='python scripts',
+    long_description='',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7'
+)

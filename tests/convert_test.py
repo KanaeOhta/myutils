c3c79be08629cd11805a864265d1843d60a63c1e
taKana671 <kanae5321@gmail.com>
2020-12-13 23:40:06 +0900

    Delete replace test because replace method was changed.

--- tests/convert_test.py
+++ tests/convert_test.py
@@ -21,15 +21,14 @@ def setUpModule():
             with open(file_path, 'w', encoding='utf-8') as f:
                 json.dump([val], f, ensure_ascii=False)
                 f.flush()
-    for json_file in glob.glob(f'{TEST_DIR.name}/*.json'):
+    for json_file in glob.glob(f'{test_path}/*.json'):
             to_excel = ToExcel(json_file)
             to_excel.convert()
-    for excel_file in glob.glob(f'{TEST_DIR.name}/*.xlsx'):
+    for excel_file in glob.glob(f'{test_path}/*.xlsx'):
         from_excel = FromExcel(excel_file)
-        from_excel.convert(
-            replacement={'a_a': 'a-a', 'c_c': 'c.c', 'd_d': 'd-d'})
+        from_excel.convert()
+          
    
-
 def tearDownModule():
     TEST_DIR.cleanup()
 
@@ -37,7 +36,7 @@ def tearDownModule():
 class ToExceTestCase(TestCase):
 
     def get_test_files(self, file_name):
-        for json_file in glob.glob(f'{TEST_DIR.name}/*.json'):
+        for json_file in glob.glob(f'{Path(TEST_DIR.name)}/*.json'):
             if '_' not in (base := os.path.splitext(os.path.basename(json_file))[0]) \
                     and base.startswith(file_name):
                 str_num = base.replace(file_name, '')
@@ -74,11 +73,10 @@ class ToExceTestCase(TestCase):
                     self.assertEqual(expect, result)
 
 
-
 class FromExcelTestCase(TestCase):
 
     def get_test_files(self, prefix):
-        for json_file in glob.glob(f'{TEST_DIR.name}/*.json'):
+        for json_file in glob.glob(f'{Path(TEST_DIR.name)}/*.json'):
             if '_' in (file_name := os.path.basename(json_file)) \
                     and file_name.startswith(prefix):
                 obj_name = file_name.split('_')[0]
@@ -104,8 +102,7 @@ dic7 = {'e': [{'f': 5, 'g': 6}, {'f': 100, 'g': 120}], 'h': [{'i': 5, 'j': 6}, {
 dic8 = {'h': [{'i': {'k': [5, 6], 'l': 100}, 'j': 6}, {'i': {'k': [7, 8], 'l': 150}, 'j': 120}]}
 dic9 = {'c': [{'f': 5, 'g': [{'h': 100, 'i': 120}, {'h': 200, 'i': 220}]}, {'f': 7, 'g': [{'h': 150, 'i': 180}]}]}
 dic10 = {'c': [{'f': 5, 'g': [{'h': 100, 'i': {'j': 15, 'k':16}}, {'h': 200, 'i': {'j': 17, 'k':18}}]}, {'f': 7, 'g': [{'h': 150, 'i': {'j': 19, 'k':20}}]}]}
-dic11 = {'a-a': 1, 'c.c': {'a': 2, 'b': {'x': 5, 'y': 10}}, 'd-d': [1, 2, 3]}
-dic12 = {'d': [[{'h': 5, 'i': 10}, {'h': 50, 'i': 1000}], [{'h': 50, 'i': 30}, {'h': 80, 'i': 100}]]}
+dic11 = {'d': [[{'h': 5, 'i': 10}, {'h': 50, 'i': 1000}], [{'h': 50, 'i': 30}, {'h': 80, 'i': 100}]]}
 
 serialized1 = {('a', '1'): 1, ('c.a', '1'): 2, ('c.b.x', '1'): 5, ('c.b.y', '1'): 10, ('d-0', '1'): 1, ('d-1', '1'): 2, ('d-2', '1'): 3}
 serialized2 = {('e.f', '1-0'): 5, ('e.g', '1-0'): 6, ('e.f', '1-1'): 100, ('e.g', '1-1'): 120}
@@ -122,9 +119,7 @@ serialized9 = {('c.f', '1-0'): 5, ('c.g.h', '1-0-0'): 100, ('c.g.i', '1-0-0'): 1
 serialized10 = {('c.f', '1-0'): 5, ('c.g.h', '1-0-0'): 100, ('c.g.i.j', '1-0-0'): 15, ('c.g.i.k', '1-0-0'): 16, 
     ('c.g.h', '1-0-1'): 200, ('c.g.i.j', '1-0-1'): 17, ('c.g.i.k', '1-0-1'): 18,
     ('c.f', '1-1'): 7, ('c.g.h', '1-1-0'): 150, ('c.g.i.j', '1-1-0'): 19, ('c.g.i.k', '1-1-0'): 20} 
-serialized11 = {('a_a', '1'): 1, ('c_c.a', '1'): 2, ('c_c.b.x', '1'): 5, 
-    ('c_c.b.y', '1'): 10, ('d_d-0', '1'): 1, ('d_d-1', '1'): 2, ('d_d-2', '1'): 3}
-serialized12 = {('d-0.h', '1-0'): 5, ('d-0.i', '1-0'): 10, ('d-0.h', '1-1'): 50, ('d-0.i', '1-1'): 1000,
+serialized11 = {('d-0.h', '1-0'): 5, ('d-0.i', '1-0'): 10, ('d-0.h', '1-1'): 50, ('d-0.i', '1-1'): 1000,
     ('d-1.h', '1-0'): 50, ('d-1.i', '1-0'): 30, ('d-1.h', '1-1'): 80, ('d-1.i', '1-1'): 100}
 
 parsed1 = {'a': ExcelSheet.MAIN, 'c.a': ExcelSheet.MAIN, 'c.b.x': ExcelSheet.MAIN, 'c.b.y': ExcelSheet.MAIN, 'd-0': ExcelSheet.MAIN,
@@ -138,9 +133,7 @@ parsed7 = {'e.f': 'e', 'e.g': 'e', 'h.i': 'h', 'h.j': 'h'}
 parsed8 = {'h.i.k-0': 'h', 'h.i.k-1': 'h', 'h.i.l': 'h', 'h.j': 'h'}
 parsed9 = {'c.f': 'c', 'c.g.h': 'c.g', 'c.g.i': 'c.g'}
 parsed10 = {'c.f': 'c', 'c.g.h': 'c.g', 'c.g.i.j': 'c.g', 'c.g.i.k': 'c.g'} 
-parsed11 = {'a_a': ExcelSheet.MAIN, 'c_c.a': ExcelSheet.MAIN, 'c_c.b.x': ExcelSheet.MAIN, 
-    'c_c.b.y': ExcelSheet.MAIN, 'd_d-0': ExcelSheet.MAIN, 'd_d-1': ExcelSheet.MAIN, 'd_d-2': ExcelSheet.MAIN}
-parsed12 = {'d-0.h': 'd-0', 'd-0.i': 'd-0', 'd-1.h': 'd-1', 'd-1.i': 'd-1'}
+parsed11 = {'d-0.h': 'd-0', 'd-0.i': 'd-0', 'd-1.h': 'd-1', 'd-1.i': 'd-1'}
 
 selected_test = {
     # value is a tuple of selected keys and expected value

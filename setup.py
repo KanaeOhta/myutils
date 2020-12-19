713ec00611c98407ad2ce7799181364daf6a5bc2
Kanae <48859041+taKana671@users.noreply.github.com>
2020-12-19 21:45:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='jsonexel',
+    name='jsonexcel',
     version='0.0.1',
     author='Kanae Ohta',
     author_email='kanae5321@gmail.com',
